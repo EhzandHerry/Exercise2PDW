72a440d9dad360b54e1cb63ba65ed143146873c8
EhzandHerry <115150312+EhzandHerry@users.noreply.github.com>
2023-06-19 22:41:23 +0700

    Add files via upload
    done

---
+++ exe2.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("Ini video Threesixty");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini audio Threesixty");
+})
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar Threesixty");
+});
